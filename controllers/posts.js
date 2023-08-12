a93deb68823c229d32be502a6bf5c0dcd5247b1a
Ernst Thelifort <ethelifort@gmail.com>
2023-08-12 19:57:57 -0400

    updated log in routes

--- controllers/posts.js
+++ controllers/posts.js
@@ -100,7 +100,7 @@ module.exports = {
       // Delete post from db
       await Post.remove({ _id: req.params.id });
       console.log("Deleted Post");
-      res.redirect("/profile");
+      res.redirect("/feed");
     } catch (err) {
       res.redirect("/profile");
     }
